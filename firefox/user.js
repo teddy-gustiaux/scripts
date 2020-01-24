ff04ba3361b175b1a0bcdd43d4cdf59a68c2f64e
Teddy Gustiaux <code@gustiaux.com>
2020-01-24 17:47:48 -0500

    add: firefox configuration - display complete URLs

--- firefox/user.js
+++ firefox/user.js
@@ -2,3 +2,5 @@
 user_pref("extensions.pocket.enabled", false);
 // Show IDN Punycode
 user_pref("network.IDN_show_punycode", true);
+// Display complete URLs
+user_pref("browser.urlbar.trimURLs", false);
