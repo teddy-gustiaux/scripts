3ae03e190798b24e8a424bd8459c0e1b1bd97703
Teddy Gustiaux <code@gustiaux.com>
2020-01-26 19:27:59 -0500

    add: Batch script to find corrupted videos

---
+++ batch/find_corrupted_videos.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SETLOCAL
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+SET counter=0
+
+FOR %%x IN (*.mkv,*.mp4) DO (
+    ECHO [WORK] Testing "%%x"
+    ffprobe "%%x" > nul 2>&1
+    IF errorlevel 1 (
+        SET listOfCorruptedFiles[!counter!]="%%x"
+        SET /A counter=counter+1
+    )
+)
+
+ECHO [INFO] Found %counter% corrupted files
+FOR /F "tokens=2 delims==" %%s IN ('SET listOfCorruptedFiles[') DO ECHO [CORRUPTED] %%s
+
+ECHO [INFO] All operations are completed
+ENDLOCAL
