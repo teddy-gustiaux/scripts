4e24dfa245dd41df5f8db6a7f8efe2c505e9b949
Teddy Gustiaux <code@gustiaux.com>
2024-01-01 13:24:27 -0500

    Add: script to merge MP4 and SRT files into an MKV

---
+++ wsl/merge_mp4_and_srt_in_mkv.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+declare -a args
+
+for directory in */ ; do
+    echo "[INFO] Going into [$directory]"
+    cd "$directory" || exit 1
+
+    for mp4 in *.mp4 ; do
+    base="${mp4%mp4}"
+    output="${base// /_}"
+    args=(-o "output/${output}mkv" "${mp4}")
+
+    if [[ -f "${base}srt" ]]; then
+        args=("${args[@]}" "${base}srt")
+    fi
+
+    echo "[INFO] Processing [$mp4]"
+
+    echo "mkvmerge.exe" "${args[@]}"
+
+    echo "[INFO] Completed [$mp4]"
+    cd ..
+
+    done
+done
